d93631c156600ef79cc977b503a2cd15ae076502
FriedemannClaus <FriedemannClaus@users.noreply.github.com>
2022-06-29 09:51:54 +0200

    Update DoubleLinkedList.java
    forgot to save the file

--- labor_loesung/src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
+++ labor_loesung/src/de/hska/iwi/ads/solution/list/DoubleLinkedList.java
@@ -11,5 +11,23 @@ import de.hska.iwi.ads.dictionary.AbstractDoubleLinkedList;
  */
 public class DoubleLinkedList<K extends Comparable<K>, V> extends AbstractDoubleLinkedList<K, V> {
 
+	@SuppressWarnings("unchecked")
+	public V get(Object o) {
+		o = (K) o;
+		ListElement listElement = search(o);
+		listElement == null ? return null : 
+		return search(o);
+	}
 	
+	public V put(K key, V value) {
+		if(search(key) != null) {
+			
+		}
+		return null;
+	}
+	
+	private ListElement search(K key) {
+		
+		return null;
+	}
 }
